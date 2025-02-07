2348131fd95f86d0c288a37246d75c6a65950ba1
DennyDaVjncj <daVjncjCode@gmail.com>
2025-02-07 00:11:03 -0500

    trustline update

--- trustless/0Trust.js
+++ trustless/0Trust.js
@@ -1,6 +1,6 @@
 const trustSet_tx = {
     "TransactionType": "TrustSet",
-    "Account": standbyDestinationField.value,
+    "Account": standbyDestinationField.value,//this config exists because, this wallet can hold a single asset
     "LimitAmount": {
       "currency": standbyCurrencyField.value,
       "issuer": standby_wallet.address,
