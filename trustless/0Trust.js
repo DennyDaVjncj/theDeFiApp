fe1f1801f50652ec6fa6a0bf0323c70e41dd6550
DennyDaVjncj <daVjncjCode@gmail.com>
2025-02-08 14:30:32 -0500

    updated logic for sending currency

--- trustless/0Trust.js
+++ trustless/0Trust.js
@@ -7,3 +7,17 @@ const trustSet_tx = {
       "value": standbyAmountField.value
     }
   }
+
+/**
+ * logic for sending tokens, via an established trustline
+ */
+const send_token_tx = {
+  "TransactionType": "Payment",
+  "Account": standby_wallet.address,//source of payment?
+  "Amount": {
+    "currency": standbyCurrencyField.value,
+    "value": standbyAmountField.value,
+    "issuer": standby_wallet.address
+  },
+  "Destination": standbyDestinationField.value
+}
