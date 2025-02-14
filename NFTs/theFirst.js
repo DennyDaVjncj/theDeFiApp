dff2084483716d9d49a3a100eb3563a22b228b0a
DennyDaVjncj <daVjncjCode@gmail.com>
2025-02-13 22:11:46 -0500

    minting, inventory & burning features configured

--- NFTs/theFirst.js
+++ NFTs/theFirst.js
@@ -1,3 +1,4 @@
+//this logic mints an NFT on the XRPL
 const transactionJson = {
     "TransactionType": "NFTokenMint",
     "Account": standby_wallet.classicAddress,
@@ -5,5 +6,24 @@ const transactionJson = {
     "Flags": parseInt(standbyFlagsField.value),//can be set to 8, to make nft transferable
     "TransferFee": parseInt(standbyTransferFeeField.value),//percentage of royaltee fee
     "NFTokenTaxon": 0 // Required, but if you have no use for it, set it to zero.
-  }
+  };
   const tx = await client.submitAndWait(transactionJson, { wallet: standby_wallet} )
+  
+  //invoking list of nfts owned by this account
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: standby_wallet.classicAddress,
+    // ledger_index: "validated"
+  });
+
+  //burning an nft!
+  const transactionBlob={
+    "TransactionType": "NFTokenBurn",
+    "Account": standby_wallet.classicAddress,
+    "NFTokenID": standbyTokenIdField.value
+}
+const tx=await client.submitiAndWait(transactionBlob,{wallet:standby_wallet});  
+
+  /**
+   * the above logic is primitive to the ledger
+*/
